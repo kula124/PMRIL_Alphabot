202fb69ff44961b9eccac16d851c5a04878665e5
kula <kula124@gmail.com>
2020-12-05 21:27:23 +0100

    eslint applied

--- src/RobotControl/controllers/mainController.js
+++ src/RobotControl/controllers/mainController.js
@@ -1,18 +1,18 @@
 const motorController = require('./motorController')
 
 const delay = duration => new Promise((resolve, reject) => {
-    setTimeout(resolve, duration)
+  setTimeout(resolve, duration)
 })
 
 const testFunction = async () => {
-    motorController.straightForward()
-    await delay(1000);
-    motorController.stop()
-    await delay(1000)
-    motorController.setInverted(true)
-    motorController.straightForward()
-    await delay(1000)
-    motorController.stop()
+  motorController.straightForward()
+  await delay(1000)
+  motorController.stop()
+  await delay(1000)
+  motorController.setInverted(true)
+  motorController.straightForward()
+  await delay(1000)
+  motorController.stop()
 }
 
 module.exports = testFunction
